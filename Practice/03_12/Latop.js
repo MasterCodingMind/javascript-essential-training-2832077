dd96ae31d12deb0204314596b74f5e34ea59888f
Master <uday.valapadasu8167@gmail.com>
2022-07-13 18:47:35 +0530

    class and objects modified

--- Practice/03_12/Latop.js
+++ Practice/03_12/Latop.js
@@ -13,3 +13,7 @@ const Latop = class {
     }
 };
 export default Latop;
+
+// A module in JavaScript is just a file containing related code. In JavaScript, we use the import
+//  and export keywords to share and receive functionalities respectively across different modules
+// . The export keyword is used to make a variable, function, class or object accessible to other modules.
